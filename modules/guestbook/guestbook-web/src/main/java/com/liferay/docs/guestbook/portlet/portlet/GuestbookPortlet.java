6a1d517a6fb333b39bc4ad6362bbcbb52983f12b
Marcos1327 <giovannygigi@gmail.com>
2023-11-21 20:52:48 -0300

    change display category for social

--- modules/guestbook/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookPortlet.java
+++ modules/guestbook/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookPortlet.java
@@ -14,14 +14,17 @@ import org.osgi.service.component.annotations.Component;
 @Component(
 	immediate = true,
 	property = {
-		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.display-category=category.social",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
-		"com.liferay.portlet.instanceable=true",
+		"com.liferay.portlet.instanceable=false",
+		"com.liferay.portlet.scopeable=true",
+		"javax.portlet.expiration-cache=0",
 		"javax.portlet.display-name=Guestbook",
 		"javax.portlet.init-param.template-path=/",
-		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.init-param.view-template=/guestbook/view.jsp",
 		"javax.portlet.name=" + GuestbookPortletKeys.GUESTBOOK,
 		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.supports.mime-type=text/html",
 		"javax.portlet.security-role-ref=power-user,user"
 	},
 	service = Portlet.class
